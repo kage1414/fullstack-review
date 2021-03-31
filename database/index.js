1a6936e146473224a5590eecac2453f93bae4a37
kage1414 <kylejohnson92294@gmail.com>
2021-03-31 00:03:13 -0400

    Add filter function, send to client as html

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  _id: Number,
+  _id: {type: Number, unique: true, dropDups: true},
   name: String,
   fullName: String,
   owner: String,
@@ -17,12 +17,40 @@ let repoSchema = mongoose.Schema({
 let Repo = mongoose.model('Repo', repoSchema, 'repos');
 
 let save = (repos) => {
-  return Repo.bulkWrite(
-    repos.map((repo) =>
-      ({
-        updateOne: {
-          filter: { _id: repo.id },
-          update: {
+
+  for (let i = 0; i < repos.length; i++) {
+    let repo = repos[i];
+
+    Repo.find({ _id: repo.id })
+      .then((result) => {
+        if (result.length > 0) {
+
+          result.forEach((ele) => {
+
+            let existingUpdate = Date.parse(ele._doc.updatedAt);
+            let currentUpdate = Date.parse(repo.updated_at);
+
+
+            if (currentUpdate > existingUpdate) {
+
+              Repo.deleteOne({ _id: ele._doc.id });
+              let doc = new Repo({
+                _id: repo.id,
+                name: repo.name,
+                fullName: repo.full_name,
+                owner: repo.owner.login,
+                ownerId: repo.owner.id,
+                url: repo.url,
+                forks: repo.forks,
+                openIssues: repo.open_issues,
+                updatedAt: repo.updated_at
+              });
+
+              doc.save();
+            }
+          });
+        } else {
+          let doc = new Repo({
             _id: repo.id,
             name: repo.name,
             fullName: repo.full_name,
@@ -32,30 +60,20 @@ let save = (repos) => {
             forks: repo.forks,
             openIssues: repo.open_issues,
             updatedAt: repo.updated_at
-          },
-          upsert: true
+          });
+          doc.save();
         }
-      }
-      ))
-  )
-    .then((result) => {
-      return result;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      });
+
+  }
+
 };
 
-let findAll = () => {
-  return Repo.find({})
-    .then((repos) => {
-      return repos;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+let getFindAllPromise = () => {
+  let query = Repo.find();
+  return query;
 };
 
-module.exports.findAll = findAll;
+module.exports.getFindAllPromise = getFindAllPromise;
 
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -1,31 +1,27 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 let app = express();
-const helpers = require('../helpers/github');
-const db = require('../database/index');
+
+const Controller = require('./controller');
+
+const controller = new Controller();
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyparser.urlencoded());
 app.use(bodyparser.json());
 
 app.post('/repos', function (req, res) {
-  helpers.getReposByUsername(req.body.username)
-    .then((response) => {
-      return db.save(response.data);
-    })
-    .then((response) => {
-      res.redirect('/repos');
-    })
-    .catch((err) => {
-      console.log(err);
+  controller.reposPost(req.body.username);
+  controller.reposGet()
+    .exec((err, repos) => {
+      let top25 = controller.filterTop25(repos);
+      let html = controller.toHTML(top25)
+      res.send(html);
     });
 });
 
 app.get('/repos', function (req, res) {
-  let allRepos = db.findAll();
-  console.log('all', allRepos);
-  // This route should send back the top 25 repos
-  res.send(allRepos);
+
 });
 
 let port = 1128;
