cbb1423b8b69d9c80cb4767371b4ce50ee694cb2
kage1414 <kylejohnson92294@gmail.com>
2021-04-03 20:29:58 -0400

    Change mongo url

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const Promise = require('bluebird');
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost/fetcher', {
+mongoose.connect(process.env.mongoUrl, {
   useMongoClient: true
 });
 
