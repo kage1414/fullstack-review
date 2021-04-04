fda8d0299f9ae3aa6df0518524c6ff47e608ad05
kage1414 <kylejohnson92294@gmail.com>
2021-04-03 20:52:36 -0400

    Express.static

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const path = require('path');
 const filePath = path.join(__dirname, '/../client/dist');
 console.log('filePath', filePath);
 
-app.use('/', express.static(filePath));
+app.use(express.static(filePath));
 app.use(bodyparser.urlencoded());
 app.use(bodyparser.json());
 
